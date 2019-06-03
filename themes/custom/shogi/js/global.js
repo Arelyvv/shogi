66852971c70509f726c3626df82ea2f89c28dfbb
Darlen <darlene.velasquez@fhios.es>
2019-06-03 16:09:06 +0200

    Primer commit

---
+++ themes/custom/shogi/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.shogi = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
