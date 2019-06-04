da9d1488cfea017987dd5a502702b762080e6d8e
Unknown <darlene.velasquez@fhios.es>
2019-06-04 11:36:45 +0200

    footer css y iconos

---
+++ sites/default/files/js/js_HPEjkkl6MoogCgH1V19GsmMZG51OJdnBu8Czal8rGI0.js
@@ -0,0 +1,66 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.shogi = {
+    attach: function (context, settings) {
+      $("#footer-link li.dropdown").click(function (eventoo) {
+        if (screen.width > 992) {
+          $(this).children('a').removeAttr("data-toggle");
+          window.location = $(this).children('a').attr("href");
+        }
+      });
+    }
+  };
+
+})(jQuery, Drupal);
+;
+/**
+ * @file
+ * Affix for Bootstrap 4.
+ * https://www.codeply.com/users/skelly
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.bootstrap_barrio_affix = {
+    attach: function (context, settings) {
+      var toggleAffix = function(affixElement, scrollElement, wrapper) {
+  
+        var height = affixElement.outerHeight(),
+            top = wrapper.offset().top;
+    
+        if (scrollElement.scrollTop() >= top){
+            wrapper.height(height);
+            affixElement.addClass("affix");
+        }
+        else {
+            affixElement.removeClass("affix");
+            wrapper.height('auto');
+        }
+      
+      };
+
+      $('[data-toggle="affix"]').once().each(function() {
+        var ele = $(this),
+            wrapper = $('<div></div>');
+    
+        ele.before(wrapper);
+        $(window).on('scroll resize', function() {
+            toggleAffix(ele, $(this), wrapper);
+        });
+    
+        // init
+        toggleAffix(ele, $(window), wrapper);
+      });
+    }
+  }
+})(jQuery, Drupal);
+;
--- themes/custom/shogi/js/global.js
+++ themes/custom/shogi/js/global.js
@@ -9,7 +9,12 @@
 
   Drupal.behaviors.shogi = {
     attach: function (context, settings) {
-
+      $("#block-shogi-main-menu li.dropdown").click(function (eventoo) {
+        if (screen.width > 992) {
+          $(this).children('a').removeAttr("data-toggle");
+          window.location = $(this).children('a').attr("href");
+        }
+      });
     }
   };
 
